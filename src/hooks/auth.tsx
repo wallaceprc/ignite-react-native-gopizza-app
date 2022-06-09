b4ec221ba6f1ec374fbd249460b02af4e5849b0f
Wallace Rocha <wallacerochascom@gmail.com>
2022-06-09 16:56:13 -0300

    first commit

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,85 @@
+import React, { createContext, useContext, ReactNode, useState } from 'react';
+
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+
+type User = {
+  id: string;
+  name: string;
+  isAdmin: boolean;
+}
+
+type AuthContextData = {
+  signIn: (email: string, password: string) => Promise<void>;
+  isLogging: boolean;
+  user: User | null;
+}
+
+type AuthProviderProps = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLogging, setIsLogging] = useState(false);
+
+  async function signIn(email: string, password: string) {
+    if (!email || !password) {
+      return Alert.alert('Login', 'Informe o e-mail e a senha')
+    }
+
+    setIsLogging(true);
+
+    auth().signInWithEmailAndPassword(email, password)
+    .then(account => {
+      firestore()
+      .collection('users')
+      .doc(account.user.uid)
+      .get()
+      .then(profile => {
+        const { name, isAdmin } = profile.data() as User;
+
+        if (profile.exists) {
+          const userData = {
+            id: account.user.uid,
+            name,
+            isAdmin
+          };
+
+          console.log(userData);
+          setUser(userData);
+        }
+      })
+    })
+    .catch(error => {
+      const { code } = error;
+
+      if (code === 'auth/user-not-found' || code === 'auth/wrong-password') {
+        return Alert.alert('Login', 'E-mail e/ou senha inválida');
+      } else {
+        return Alert.alert('Login', 'Não foi possível realizar o login');
+      }
+    })
+    .finally(() => setIsLogging(false));
+  }
+
+  return(
+    <AuthContext.Provider value={{
+      signIn,
+      isLogging
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth() {
+  const context = useContext(AuthContext)
+
+  return context;
+}
+
+export { AuthProvider, useAuth }
